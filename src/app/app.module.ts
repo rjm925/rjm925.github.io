7a82cc97589277ee74857eaf35e09203e91a53bb
Ryan Mendoza <ryanmendoza925@gmail.com>
2017-11-12 19:49:38 -0500

    layout/styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { ContactComponent } from './components/contact/contact.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 const appRoutes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'portfolio', component:PortfolioComponent},
-  {path: 'contact', component:ContactComponent}
 ]
 
 @NgModule({
@@ -22,7 +20,6 @@ const appRoutes: Routes = [
     NavbarComponent,
     HomeComponent,
     PortfolioComponent,
-    ContactComponent,
     SidebarComponent,
     FooterComponent
   ],
